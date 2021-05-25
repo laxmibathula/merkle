acd7af45f8de8693a9a14ebb10a3c05e2c934185
laxmibathula <laxmi.bathula4@gmail.com>
2021-05-25 01:53:29 -0400

    deleted log

--- routes.js
+++ routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const users = require("./users-schema");
 
 router.post("/register", async (req, res) => {
-   console.log(req.body);
    const user = new users({
       firstname: req.body.details.firstName,
       lastname: req.body.details.lastName,
